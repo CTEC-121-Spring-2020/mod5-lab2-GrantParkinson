d42ee6dfb4076c963c30ba41a2e1929d2a0942f2
Grant Parkinson <grant.c.parkinson@gmail.com>
2020-05-20 12:14:56 -0700

    finished class

--- Template.py
+++ Template.py
@@ -1,6 +1,6 @@
 """
 CTEC 121
-<your name>
+<Grant Parkinson>
 <assignment/lab name>
 <assignment/lab description
 """
@@ -11,7 +11,55 @@ Process: description of what function does
 Output: return value and description
 """
 
-def main():
-    # code here
+'''
+def song(name):
+    print(name, "finger,", name, "finger, where are you?")
+    print("Here I am, here I am. How do you do?\n")
 
-main()    
+
+def sing():
+    song("Daddy")
+    song("Mommy")
+    song("Brother")
+    song("Sister")
+    song("Baby")
+
+
+def main():
+    sing()
+
+'''
+
+
+def age_input(age):
+
+    if 0 <= age <= 1:
+        return 'I'
+    elif 1 <= age < 13:
+        return 'C'
+    elif 13 <= age < 18:
+        return 'T'
+    elif 18 <= age < 123:
+        return 'A'
+    else:
+        return 'Input a valid age range'
+
+
+def unit_test():
+    print(age_input(1))
+    print(age_input(6))
+    print(age_input(15))
+    print(age_input(50))
+    print(age_input(-12))
+    print(age_input(200))
+    print()
+
+
+unit_test()
+
+
+def main():
+    print(age_input(1))
+
+
+main()
